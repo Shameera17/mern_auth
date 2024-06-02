9c9d6db717d0d2fa994db67b8e7a14629653d930
Shameera17 <shameera.carrim17@gmail.com>
2024-06-02 18:52:40 +0530

    connected to  db

--- api/server.js
+++ api/server.js
@@ -1,5 +1,11 @@
+import dotenv from "dotenv";
 import express from "express";
-
+import mongoose from "mongoose";
+dotenv.config();
+mongoose
+  .connect(process.env.DBURI)
+  .then(() => console.log("connected to db"))
+  .catch((e) => console.log("error connecting to db", e));
 const app = express();
 
 app.listen(3000, () => console.log("server listening on port 3000!"));
