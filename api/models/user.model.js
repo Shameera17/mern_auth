b0d0a4defe360002622d7f79f6839db1bd424e1d
Shameera17 <shameera.carrim17@gmail.com>
2024-06-02 19:07:52 +0530

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("User", userSchema);
+export default User;
