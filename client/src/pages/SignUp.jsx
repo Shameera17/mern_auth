3ca86b56cb57b1e260756cd4375b16e810051988
Shameera17 <shameera.carrim17@gmail.com>
2024-06-20 07:00:14 +0530

    added sign up functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 export default function SignUp() {
+  const [loading, setloading] = useState(false);
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState("false");
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setloading(true);
+
+      const response = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      await response.json().then((d) => {
+        setloading(false);
+        if (!d.success) {
+          setError(true);
+        }
+      });
+    } catch (error) {
+      setloading(false);
+      setError(true);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           name=""
           placeholder="Username"
           id="username"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          name=""
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="password"
@@ -18,19 +54,15 @@ export default function SignUp() {
           placeholder="Password"
           id="password"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
-        <input
-          type="text"
-          name=""
-          placeholder="Username"
-          id="username"
-          className="bg-slate-100 p-3 rounded-lg"
-        />
+
         <button
+          disabled={loading}
           className="bg-slate-700 rounded-lg p-3 text-white uppercase hover:opacity-95 disabled:opacity-80"
           type="submit"
         >
-          Sign up
+          {loading ? "Loading..." : "Sign up"}
         </button>
       </form>
       <div className="flex  gap-2 mt-5">
@@ -39,6 +71,7 @@ export default function SignUp() {
           <span className="text-blue-500">Sign In</span>
         </Link>
       </div>
+      <p className="text-red-700">{error && "Something went wrong"}</p>
     </div>
   );
 }
